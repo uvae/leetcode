77acc1afb13abf91fe8a4792cb6ba554d4e10ffc
Jacob <dlssdaa@gmail.com>
2025-03-19 17:23:20 +0900

    Time: 3 ms (72.88%), Space: 18 MB (36.16%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,21 +6,20 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s: Optional[ListNode] = None
-        l: Optional[ListNode] = None
-        c = False
+        dummy_head = ListNode()  # 더미 노드 생성
+        cur = dummy_head  # 현재 위치 포인터
+        carry = 0  # 올림수 초기화
 
-        while(l1 or l2 or c):
-            v = (l1.val if l1 else 0) + (l2.val if l2 else 0) + (1 if c else 0)
-            if(s):
-                l.next = ListNode(v%10, None)
-                l = l.next
-            else:
-                s = ListNode(v%10, next=None)
-                l = s
-            
-            c = True if v >= 10 else False
-            l1 = l1.next if l1 and l1.next else None
-            l2 = l2.next if l2 and l2.next else None
-        
-        return s
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            total = v1 + v2 + carry
+
+            carry = total // 10  # 10 이상의 값이면 올림 발생
+            cur.next = ListNode(total % 10)  # 한 자리 숫자만 저장
+            cur = cur.next  # 다음 노드로 이동
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy_head.next  # 첫 번째 노드 반환
