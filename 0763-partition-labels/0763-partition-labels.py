7ce02271d399181b94ba00a7b89eb21621fb2a02
Jacob <dlssdaa@gmail.com>
2025-03-30 23:14:42 +0900

    Time: 4 ms (65.08%), Space: 17.8 MB (36.45%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        partition = []
+        start, end = 0, 0
+        
+        last = {c: i for i, c in enumerate(s)}
+        for i, c in enumerate(s):
+            end = max(end, last[c]) # 현재 루프 중 더 뒤에 끝나는 문자열이 있으면, 지점을 옮김
+            if(i == end): # 만약 루프 중 더이상 뒷 지점에 있는거 없는 경우 (모든 문자열이 포함된게 뒤에선 더 이상 안나옴)
+                partition.append(end - start + 1) # 시작점에서 얼마나 오면 되는지 알림
+                start = i + 1
+        
+        return partition
